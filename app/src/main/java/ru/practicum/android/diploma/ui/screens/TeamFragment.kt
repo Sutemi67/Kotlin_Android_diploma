4805a0ab5ff4d87869d1cc9d635dfcd139af4597
SergeyBoykov <sutemi67@gmail.com>
2025-05-23 14:37:19 +0300

    навигация и заглушки экранов

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/TeamFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/TeamFragment.kt
@@ -9,10 +9,10 @@ import ru.practicum.android.diploma.R
 
 class TeamFragment : Fragment() {
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_team, container, false)
     }
 }
