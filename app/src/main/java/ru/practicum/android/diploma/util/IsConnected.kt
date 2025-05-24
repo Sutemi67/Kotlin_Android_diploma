cdcfcd1c03a90ea9d802255610fca2425ebdcb51
Tima <tsuahnov26@gmail.com>
2025-05-24 11:31:22 +0500

    Добавил вспомогательные классы и функции

--- app/src/main/java/ru/practicum/android/diploma/util/IsConnected.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/IsConnected.kt
@@ -7,12 +7,12 @@ import android.net.NetworkCapabilities
 fun isConnected(context: Context): Boolean {
     val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     val capabilities = connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
-    if (capabilities != null) {
-        when {
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> return true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> return true
-            capabilities.hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET) -> return true
-        }
-    }
+
+    val isConnected = capabilities?.run {
+        hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+            hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
+            hasTransport(NetworkCapabilities.TRANSPORT_ETHERNET)
+    } ?: false
+
     return false
 }
