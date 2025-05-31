8d345e979052ae863e07b06fa838279a4e6527e7
SergeyBoykov <sutemi67@gmail.com>
2025-05-31 19:16:53 +0300

    падежи вакансий

--- app/src/main/java/ru/practicum/android/diploma/util/AppFormatters.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/AppFormatters.kt
@@ -1,11 +1,16 @@
 package ru.practicum.android.diploma.util
 
 object AppFormatters {
+    const val ONE = 1
+    const val TWO = 2
+    const val FOUR = 4
+    const val TEN = 10
+
     fun vacanciesCountTextFormatter(count: String): String {
         val count = count.toInt()
-        return when (count % 10) {
-            1 -> "Найдена $count вакансия"
-            in 2..4 -> "Найдено $count вакансии"
+        return when (count % TEN) {
+            ONE -> "Найдена $count вакансия"
+            in TWO..FOUR -> "Найдено $count вакансии"
             else -> "Найдено $count вакансий"
         }
     }
