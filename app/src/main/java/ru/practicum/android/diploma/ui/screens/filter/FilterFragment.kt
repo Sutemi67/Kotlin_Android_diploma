e16ba8ab6a19c3bf7e69da57d574376075b8bb4b
SergeyBoykov <sutemi67@gmail.com>
2025-06-07 14:45:07 +0300

    добавлен сброс ввода

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/filter/FilterFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/filter/FilterFragment.kt
@@ -1,12 +1,18 @@
 package ru.practicum.android.diploma.ui.screens.filter
 
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.text.Editable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isVisible
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFilterBinding
 
 class FilterFragment : Fragment() {
@@ -31,11 +37,49 @@ class FilterFragment : Fragment() {
         setupBindings()
     }
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun setupBindings() {
         binding.toolbar.setNavigationOnClickListener { findNavController().popBackStack() }
         binding.area.setOnClickListener { }
         binding.workingArea.setOnClickListener { }
         binding.clearButton.isVisible = true
         binding.applyButton.isVisible = true
+
+        binding.salaryInput.setOnTouchListener { _, event ->
+            if (event.action == android.view.MotionEvent.ACTION_UP) {
+                val drawableEnd = binding.salaryInput.compoundDrawablesRelative[2]
+                if (drawableEnd != null) {
+                    val touchableWidth = drawableEnd.intrinsicWidth + binding.salaryInput.paddingEnd
+                    if (event.rawX >= (binding.salaryInput.right - touchableWidth)) {
+                        binding.salaryInput.text?.clear()
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
+        }
+
+        binding.salaryInput.addTextChangedListener(
+            onTextChanged = { text: CharSequence?, _, _, _ ->
+                binding.salaryInput.setCompoundDrawablesRelativeWithIntrinsicBounds(
+                    null,
+                    null,
+                    imageChooser(text),
+                    null
+                )
+            },
+            afterTextChanged = { _: Editable? -> }
+        )
+    }
+
+    private fun imageChooser(text: CharSequence?): Drawable? {
+        return if (text.isNullOrBlank()) {
+            null
+        } else {
+            AppCompatResources.getDrawable(
+                requireContext(),
+                R.drawable.close_24px
+            )
+        }
     }
 }
