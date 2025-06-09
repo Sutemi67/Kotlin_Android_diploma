296006138ac6973ed4ff7624f56e0a6009715952
Aleksandr.A <Alexandr.AAntipin@yandex.ru>
2025-06-09 23:49:27 +0300

    настроил региона первая часть

--- app/src/main/java/ru/practicum/android/diploma/ui/screens/filter/region/RegionsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/filter/region/RegionsFragment.kt
@@ -10,35 +10,35 @@ import ru.practicum.android.diploma.databinding.FragmentRegionBinding
 
 class RegionsFragment : Fragment() {
 
-    private var _binding: FragmentRegionBinding? = null
-    private val binding: FragmentRegionBinding get() = requireNotNull(_binding)
-
-    private val viewModel: AreasViewModel by viewModels { /* ViewModelProvider.Factory */ }
-    private lateinit var adapter: AreaAdapter
-
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: android.os.Bundle?
-    ): View? {
-        _binding = FragmentRegionBinding.inflate(inflater, container, false)
-        return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val country = /* получить из аргументов */
-            adapter = AreaAdapter(emptyList()) { region ->
-                // обработка выбора региона
-            }
-
-        binding.recyclerView.adapter = adapter
-
-        lifecycleScope.launchWhenStarted {
-            viewModel.regions.collect { list ->
-                adapter.submitList(list)
-            }
-        }
-        viewModel.loadRegions(country)
-    }
+//    private var _binding: FragmentRegionBinding? = null
+//    private val binding: FragmentRegionBinding get() = requireNotNull(_binding)
+//
+//    private val viewModel: AreasViewModel by viewModels { /* ViewModelProvider.Factory */ }
+//    private lateinit var adapter: AreaAdapter
+//
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: android.os.Bundle?
+//    ): View? {
+//        _binding = FragmentRegionBinding.inflate(inflater, container, false)
+//        return binding.root
+//    }
+//
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        val country = /* получить из аргументов */
+//            adapter = AreaAdapter(emptyList()) { region ->
+//                // обработка выбора региона
+//            }
+//
+//        binding.recyclerView.adapter = adapter
+//
+//        lifecycleScope.launchWhenStarted {
+//            viewModel.regions.collect { list ->
+//                adapter.submitList(list)
+//            }
+//        }
+//        viewModel.loadRegions(country)
+//    }
 }
