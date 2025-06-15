1c890cf637d932241a80dc47e19ae3b349b963a7
SergeyBoykov <sutemi67@gmail.com>
2025-06-15 12:34:44 +0300

    исправления

--- app/src/main/java/ru/practicum/android/diploma/domain/interactor/FilterInteractorImpl.kt
+++ app/src/main/java/ru/practicum/android/diploma/domain/interactor/FilterInteractorImpl.kt
@@ -5,7 +5,9 @@ import ru.practicum.android.diploma.domain.FilterRepository
 import ru.practicum.android.diploma.domain.network.models.Area
 import ru.practicum.android.diploma.domain.network.models.Industry
 
-class FilterInteractorImpl(private val repository: FilterRepository) : FilterInteractor {
+class FilterInteractorImpl(
+    private val repository: FilterRepository
+) : FilterInteractor {
 
     override suspend fun getIndustries(): List<Industry>? {
         return repository.getIndustries()
--- app/src/main/java/ru/practicum/android/diploma/ui/screens/filter/IndustryFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/screens/filter/IndustryFragment.kt
@@ -1,13 +1,10 @@
 package ru.practicum.android.diploma.ui.screens.filter
 
-import android.annotation.SuppressLint
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.view.isVisible
 import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
@@ -16,7 +13,6 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentIndustryBinding
 
 class IndustryFragment : Fragment() {
@@ -49,56 +45,14 @@ class IndustryFragment : Fragment() {
         setupClickListeners()
     }
 
-    @SuppressLint("ClickableViewAccessibility")
     private fun setupFilterField() {
+        binding.clearIndustry.setOnClickListener { binding.industryFilterField.text?.clear() }
         binding.industryFilterField.addTextChangedListener(
             onTextChanged = { text: CharSequence?, _, _, _ ->
                 viewModel.filterList(text)
-                if (!text.isNullOrEmpty()) {
-                    binding.industryFilterField.setCompoundDrawablesRelativeWithIntrinsicBounds(
-                        null,
-                        null,
-                        imageChooser(text),
-                        null
-                    )
-                    setClearIcon()
-                }
+                binding.clearIndustry.isVisible = !text.isNullOrEmpty()
             },
         )
-    }
-
-    @SuppressLint("ClickableViewAccessibility")
-    private fun setClearIcon() {
-        binding.industryFilterField.setOnTouchListener { _, event ->
-            if (event.action == android.view.MotionEvent.ACTION_UP) {
-                val drawableEnd = binding.industryFilterField.compoundDrawablesRelative[2]
-                if (drawableEnd != null) {
-                    val touchableWidth = drawableEnd.intrinsicWidth + binding.industryFilterField.paddingEnd
-                    val isClickOnDrawable = event.rawX >= binding.industryFilterField.right - touchableWidth
-                    if (isClickOnDrawable) {
-                        binding.industryFilterField.text?.clear()
-                        true
-                    } else {
-                        false
-                    }
-                } else {
-                    false
-                }
-            } else {
-                false
-            }
-        }
-    }
-
-    private fun imageChooser(text: CharSequence?): Drawable? {
-        return if (text.isNullOrBlank()) {
-            null
-        } else {
-            AppCompatResources.getDrawable(
-                requireContext(),
-                R.drawable.close_24px
-            )
-        }
     }
 
     private fun setupClickListeners() {
